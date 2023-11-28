fix rotation flag

recognition/SLT_API.py
@@ -25,7 +25,7 @@ app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app)
 
 CONFIG_PATH = "config.json"
 SAMPLE_LENGTH = 32
-INVERT = False
+ROTATE_180 = False
 
 frame_queue = deque(maxlen=32)
 sign_res = []
@@ -119,7 +119,7 @@ def data(sid, data):
     frame = np.frombuffer(image_bytes, dtype=np.uint8)
     image = cv2.imdecode(frame, -1)
     # if camera rotated to 180 degrees
-    if args["invert"]:
+    if ROTATE_180:
         image = cv2.rotate(image, cv2.ROTATE_180)
     users[sid][0].append(np.array(image[:, :, ::-1]))
 