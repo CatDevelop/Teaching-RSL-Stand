cc3d3acb097fe7cc6bb5e08fb26df8253c548408
CatDeveloper <maks.rozhkov.2004@gmail.com>
2023-12-01 08:24:21 +0500

    feat: change host

--- src/core/utils/connectToModal.ts
+++ src/core/utils/connectToModal.ts
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-export const socket = io('wss://pincode-dev.ru', {
+export const socket = io('ws://localhost:5000', {
     autoConnect: false,
     'reconnection': true,
     'reconnectionDelay': 500,
